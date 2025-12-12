6920d5216a8b90612a67bd8b3782974803095be0
viniciuszol <devvinicius08@gmai.com>
2025-12-12 12:33:02 -0300

    Update

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export async function loginRequest(email, password) {
-  const res = await fetch("http://localhost:8000/auth/login", {
+  const res = await fetch(`${BASE_URL}/auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
@@ -14,7 +16,7 @@ export async function loginRequest(email, password) {
 }
 
 export async function registerRequest(nome, email, password) {
-  const res = await fetch("http://localhost:8000/auth/register", {
+  const res = await fetch(`${BASE_URL}/auth/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ nome, email, password }),
